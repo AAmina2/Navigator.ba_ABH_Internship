188b75a1c51a5cb9224ffe5ff6ceb542e2b2fa68
AAmina2 <aalijagic2@etf.unsa.ba>
2018-03-26 08:34:30 +0200

    Delete selenium_example.rb

--- selenium_example.rb
+++
@@ -1,7 +0,0 @@
-require "selenium-webdriver"
-driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "http://www.navigator.ba"
-element = driver.find_element(:id, "ember564")
-element.send_keys "Ambrosia"
-driver.find_element(:class, "iconav-search").click
-
